bc026953de196bae9b5b5f5cc00d1660a5d6929c
BaptisteDombard <91158751+BaptisteDombard@users.noreply.github.com>
2021-12-07 23:47:01 +0100

    init

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,22 @@
-document.documentElement.className = "js-enabled";
+(function () {
+    const SpreadOperator = {
+        cacheDom() {
+            this.heading = document.querySelector('.jump');
+            this.aLetters = this.heading.textContent;
+        },
+        init() {
+            window.document.documentElement.classList.add('js-enabled');
+            this.cacheDom();
+            this.removeRemoveElements(this.heading);
+            for (const letter of this.aLetters) {
+                const eSpan = document.createElement('span');
+                eSpan.appendChild(document.createTextNode(letter === ' ' ? '\u00A0' : letter));
+                this.heading.appendChild(eSpan);
+            }
+        },
+        removeRemoveElements() {
+            this.heading.textContent = "";
+        }
+    };
+    SpreadOperator.init();
+})();
